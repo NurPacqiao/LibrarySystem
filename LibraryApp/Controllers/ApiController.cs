e5c150ee908cd9e55967d1166f2e32e6e7b1201d
Nurlan <nurpacqiao@gmail.com>
2026-01-14 17:37:15 +0100

    Minor fixes

--- LibraryApp/Controllers/ApiController.cs
+++ LibraryApp/Controllers/ApiController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using LibraryApp.Data;
+using LibraryApp.Models;
 
 namespace LibraryApp.Controllers
 {
@@ -15,17 +16,17 @@ namespace LibraryApp.Controllers
             _context = context;
         }
 
+
+        // 1. READ (Odczyt - to co już miałeś)
         // GET: api/Api
-        // This returns the list of books as raw JSON data
         [HttpGet]
         public async Task<IActionResult> GetBooks()
         {
             var books = await _context.Books
-                .Include(b => b.Author) // Include Author details
-                .Include(b => b.Genre)  // Include Genre details
+                .Include(b => b.Author)
+                .Include(b => b.Genre)
                 .ToListAsync();
 
-            // We select only the data we want to share to avoid loops
             var result = books.Select(b => new
             {
                 b.Id,
@@ -37,6 +38,81 @@ namespace LibraryApp.Controllers
             });
 
             return Ok(result);
+        }
+
+        // GET: api/Api/5 (Pobranie jednej książki)
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetBook(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(book);
+        }
+
+
+        // 2. CREATE (Dodawanie)
+        // POST: api/Api
+        [HttpPost]
+        public async Task<ActionResult<Book>> PostBook(Book book)
+        {
+            _context.Books.Add(book);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBook", new { id = book.Id }, book);
+        }
+
+        // 3. UPDATE (Edycja)
+        // PUT: api/Api/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBook(int id, Book book)
+        {
+            if (id != book.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(book).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Books.Any(e => e.Id == id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+
+        // 4. DELETE (Usuwanie)
+        // DELETE: api/Api/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBook(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _context.Books.Remove(book);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
